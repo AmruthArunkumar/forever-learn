c823bf11e3a0fafc20e75d37aa76abf7f78a31f6
Amruth Arunkumar <amruth1.618@gmail.com>
2026-05-03 12:20:15 -0400

    test cron

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,3 +1,15 @@
-export function GET(request: Request) {
-    return new Response('Hello from Vercel!');
+import { NextRequest } from "next/server";
+
+export function GET(request: NextRequest) {
+    const authHeader = request.headers.get('authorization');
+    const cronSecret = process.env.CRON_SECRET;
+
+    if (!cronSecret || authHeader !== `Bearer ${cronSecret}`) {
+        return new Response('Unauthorized', {
+            status: 401,
+        });
+    }
+
+    console.log("Hello World!")
+    return Response.json({message: "Success!"})
 }
